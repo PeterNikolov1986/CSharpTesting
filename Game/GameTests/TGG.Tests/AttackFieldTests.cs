0b8e14e95172276789f0d9e585bdd5fa20ad082e
PeterNikolov1986 <peter1986nikolov@gmail.com>
2019-12-03 19:08:58 +0200

    Added new test methods

--- Game/GameTests/TGG.Tests/AttackFieldTests.cs
+++ Game/GameTests/TGG.Tests/AttackFieldTests.cs
@@ -44,6 +44,22 @@
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
         }
 
+        [TestCase((int)ValidBoundaryTypes.Zero, (int)ValidBoundaryTypes.Zero)]
+        [TestCase((int)ValidBoundaryTypes.Zero, (int)ValidAverageTypes.FirstY)]
+        [TestCase((int)ValidBoundaryTypes.Zero, (int)ValidBoundaryTypes.Y)]
+        [TestCase((int)ValidAverageTypes.SecondX, (int)ValidBoundaryTypes.Zero)]
+        [TestCase((int)ValidAverageTypes.SecondX, (int)ValidAverageTypes.FirstY)]
+        [TestCase((int)ValidAverageTypes.SecondX, (int)ValidBoundaryTypes.Y)]
+        [TestCase((int)ValidBoundaryTypes.X, (int)ValidBoundaryTypes.Zero)]
+        [TestCase((int)ValidBoundaryTypes.X, (int)ValidAverageTypes.FirstY)]
+        [TestCase((int)ValidBoundaryTypes.X, (int)ValidBoundaryTypes.Y)]
+        public void UnsuccessfulAttackFieldAttempt_WithValidValues_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "The attack field's command is not successful.");
+        }
+
         [TestCase((int)ValidBoundaryTypes.Zero, (int)InvalidBoundaryTypes.Negative)]
         [TestCase((int)ValidBoundaryTypes.Zero, (int)InvalidBoundaryTypes.Y)]
         [TestCase((int)ValidAverageTypes.SecondX, (int)InvalidBoundaryTypes.Negative)]
@@ -55,11 +71,33 @@
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
         }
 
+        [TestCase((int)ValidBoundaryTypes.Zero, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)ValidBoundaryTypes.Zero, (int)InvalidBoundaryTypes.Y)]
+        [TestCase((int)ValidAverageTypes.SecondX, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)ValidAverageTypes.SecondX, (int)InvalidBoundaryTypes.Y)]
+        [TestCase((int)ValidBoundaryTypes.X, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)ValidBoundaryTypes.X, (int)InvalidBoundaryTypes.Y)]
+        public void EvenAttackFieldAttempt_WithInvalidValuesOfY_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "Invalid value of the Y coordinate.");
+        }
+
         [TestCase((int)ValidAverageTypes.FirstX, (int)InvalidBoundaryTypes.Negative)]
         [TestCase((int)ValidAverageTypes.FirstX, (int)InvalidBoundaryTypes.Y)]
         public void OddAttackFieldAttempt_WithInvalidValuesOfY_ShouldThrow_ArgumentException(int x, int y)
         {
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+        }
+
+        [TestCase((int)ValidAverageTypes.FirstX, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)ValidAverageTypes.FirstX, (int)InvalidBoundaryTypes.Y)]
+        public void OddAttackFieldAttempt_WithInvalidValuesOfY_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "Invalid value of the Y coordinate.");
         }
 
         [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidBoundaryTypes.Zero)]
@@ -73,11 +111,33 @@
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
         }
 
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidBoundaryTypes.Zero)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)ValidBoundaryTypes.Zero)]
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidAverageTypes.FirstY)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)ValidAverageTypes.FirstY)]
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidBoundaryTypes.Y)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)ValidBoundaryTypes.Y)]
+        public void EvenAttackFieldAttempt_WithInvalidValuesOfX_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "Invalid value of the X coordinate.");
+        }
+
         [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidAverageTypes.SecondY)]
         [TestCase((int)InvalidBoundaryTypes.X, (int)ValidAverageTypes.SecondY)]
         public void OddAttackFieldAttempt_WithInvalidValuesOfX_ShouldThrow_ArgumentException(int x, int y)
         {
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+        }
+
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)ValidAverageTypes.SecondY)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)ValidAverageTypes.SecondY)]
+        public void OddAttackFieldAttempt_WithInvalidValuesOfX_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "Invalid value of the X coordinate.");
         }
 
         [TestCase((int)InvalidBoundaryTypes.Negative, (int)InvalidBoundaryTypes.Negative)]
@@ -87,6 +147,19 @@
         public void AttackFieldAttempt_WithInvalidValues_ShouldThrow_ArgumentException(int x, int y)
         {
             Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+        }
+
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)InvalidBoundaryTypes.Negative)]
+        [TestCase((int)InvalidBoundaryTypes.Negative, (int)InvalidBoundaryTypes.Y)]
+        [TestCase((int)InvalidBoundaryTypes.X, (int)InvalidBoundaryTypes.Y)]
+        public void AttackFieldAttempt_WithInvalidValues_ShouldHave_AppropriateErrorMessage(int x, int y)
+        {
+            ArgumentException expression = Assert.Throws<ArgumentException>(() => this.calculator.IsAttackSuccesful(x, y));
+
+            Assert.AreEqual(expression.Message, "Invalid value of the X coordinate.");
+            Assert.AreNotEqual(expression.Message, "Invalid value of the Y coordinate.");
+            Assert.AreNotEqual(expression.Message, "The attack field's command is not successful.");
         }
     }
 }
